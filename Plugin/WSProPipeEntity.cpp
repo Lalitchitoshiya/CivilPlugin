e54ee22629c012758551d4aab30166191a0ef993
Lalitchitoshiya <Lalit.chitoshiya@cctech.co.in>
2026-01-16 01:06:15 +0530

    length converted into Pipe

--- Plugin/WSProPipeEntity.cpp
+++ Plugin/WSProPipeEntity.cpp
@@ -3,6 +3,12 @@
 #include "acgi.h"
 #include "dbfiler.h"
 #include "rxregsvc.h"
+#include <cmath> // Include cmath for mathematical functions
+
+// Define AcGePi if not already defined
+#ifndef AcGePi
+constexpr double AcGePi = 3.14159265358979323846;
+#endif
 
 ACRX_DXF_DEFINE_MEMBERS(
     WSProPipeEntity,
@@ -35,18 +41,76 @@ AcGePoint3d WSProPipeEntity::endPoint() const { return m_endPt; }
 void WSProPipeEntity::setDiameter(double dia) { m_diameter = dia; }
 double WSProPipeEntity::diameter() const { return m_diameter; }
 
+//Adesk::Boolean WSProPipeEntity::subWorldDraw(AcGiWorldDraw* wd)
+//{
+//    AcGiGeometry* geom = &wd->geometry();
+//    if (!geom) return Adesk::kFalse;
+//
+//    wd->subEntityTraits().setColor(5); // blue
+//
+//    AcGePoint3d pts[2] = { m_startPt, m_endPt };
+//    geom->polyline(2, pts);
+//
+//    return Adesk::kTrue;
+//}
+
 Adesk::Boolean WSProPipeEntity::subWorldDraw(AcGiWorldDraw* wd)
 {
     AcGiGeometry* geom = &wd->geometry();
-    if (!geom) return Adesk::kFalse;
+    if (!geom)
+        return Adesk::kFalse;
 
-    wd->subEntityTraits().setColor(5); // blue
+    // Pipe color (blue)
+    wd->subEntityTraits().setColor(5);
 
-    AcGePoint3d pts[2] = { m_startPt, m_endPt };
-    geom->polyline(2, pts);
+    // Direction vector
+    AcGeVector3d axis = m_endPt - m_startPt;
+    double length = axis.length();
+    if (length <= 1e-6)
+        return Adesk::kFalse;
+
+    axis.normalize();
+
+    // Create perpendicular vectors
+    AcGeVector3d up = axis.perpVector().normalize();
+    AcGeVector3d right = axis.crossProduct(up).normalize();
+
+    const int slices = 12;               // smoothness
+    const double radius = m_diameter / 2.0;
+
+    AcGePoint3d vertices[slices * 2];
+    Adesk::Int32 faces[slices * 5];
+
+    for (int i = 0; i < slices; ++i)
+    {
+        double angle = (2.0 * AcGePi * i) / slices;
+        AcGeVector3d offset =
+            cos(angle) * right * radius +
+            sin(angle) * up * radius;
+
+        vertices[i] = m_startPt + offset;
+        vertices[i + slices] = m_endPt + offset;
+
+        int f = i * 5;
+        int next = (i + 1) % slices;
+
+        faces[f + 0] = 4;
+        faces[f + 1] = i;
+        faces[f + 2] = next;
+        faces[f + 3] = next + slices;
+        faces[f + 4] = i + slices;
+    }
+
+    geom->shell(
+        slices * 2,           // number of vertices
+        vertices,
+        slices * 5,           // face list size
+        faces
+    );
 
     return Adesk::kTrue;
 }
+
 
 Acad::ErrorStatus WSProPipeEntity::dwgOutFields(AcDbDwgFiler* filer) const
 {
